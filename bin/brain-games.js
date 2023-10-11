1af02d88abf0142563d803736b6002fdc6b966bd
tatiana <alks.tatiana@gmail.com>
2023-10-11 21:14:03 +0700

    add eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-console.log("Welcome to the Brain Games!");
-import name from "../src/cli.js";
+import name from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
 name();
